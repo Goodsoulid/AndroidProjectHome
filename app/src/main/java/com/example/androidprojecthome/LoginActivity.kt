5df936fef7300dfa5eeaccb49cd6554ca5cacb6d
Goodsoulid <Frozon1994@gmail.com>
2022-11-19 22:45:55 +0300

    Lesson 18

--- app/src/main/java/com/example/androidprojecthome/LoginActivity.kt
+++ app/src/main/java/com/example/androidprojecthome/LoginActivity.kt
@@ -3,21 +3,21 @@ package com.example.androidprojecthome
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.androidprojecthome.databinding.ActivityMainBinding
+import com.example.androidprojecthome.databinding.ActivityLoginBinding
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityLoginBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         binding.btnLogin.setOnClickListener {
-            startActivity(Intent(this, LoginActivity::class.java))
+            startActivity(Intent(this, HomeActivity::class.java))
         }
-        binding.btnRegister.setOnClickListener {
+        binding.tvHaventAccount.setOnClickListener {
             startActivity(Intent(this, RegisterActivity::class.java))
         }
     }
